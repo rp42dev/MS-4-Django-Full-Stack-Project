9e1b1f39b5f915fe13915d89604a9cfc258f4bd1
Raivis80 <rapet80@gmail.com>
2021-11-28 14:12:07 +0000

    added shop models and addet to admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,34 @@
+
 from django.contrib import admin
+from .models import Product, Category, ProductStatus
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'special_price',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'item',
+        'item_count',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductStatus, ProductStatusAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    friendly_name = models.CharField(max_length=60, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=60)
+    description = models.TextField(max_length=250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    special_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductStatus(models.Model):
+    item = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
+    item_count = models.IntegerField(blank=False, null=False, default=0)
+
+    def __str__(self):
+        return self.name
