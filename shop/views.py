bac5fdf581605ecf9b6c30b4c1a152e26041e32e
Raivis80 <rapet80@gmail.com>
2021-11-29 22:33:08 +0000

    added related itemfunctionality

--- shop/views.py
+++ shop/views.py
@@ -17,7 +17,11 @@ def shop(request):
 def shop_item(request, item_id):
     """A view to return the shop item detailed page"""
     item = get_object_or_404(Product, pk=item_id)
+    related = Product.objects.filter(style=item.style)
+    for i in related:
+        print(i.image)
     context = {
         'item': item,
+        'related': related,
     }
     return render(request, 'shop/shop_item.html', context)
