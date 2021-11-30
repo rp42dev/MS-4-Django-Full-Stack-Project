8f3ce403b3a0b961953f0d4675e214737d1a7147
Raivis80 <rapet80@gmail.com>
2021-11-30 11:49:08 +0000

    added queries by categories and items on sale functionality

--- shop/views.py
+++ shop/views.py
@@ -6,8 +6,19 @@ from .models import Product, Category
 # Create your views here.
 def shop(request):
     """A view to return the shop page"""
-    products = Product.objects.all()
+    categories = Category.objects.all()
+    if request.GET:
+        if 'category' in request.GET:
+            cat = request.GET['category']
+            if cat == 'sale':
+                products = Product.objects.filter(sale=True)
+            elif cat == 'hats':
+                products = Product.objects.all()
+            else:
+                products = Product.objects.filter(category__name=cat)
+
     context = {
+        'categories': categories,
         'products': products,
     }
     return render(request, 'shop/shop.html', context)
@@ -18,8 +29,6 @@ def shop_item(request, item_id):
     """A view to return the shop item detailed page"""
     item = get_object_or_404(Product, pk=item_id)
     related = Product.objects.filter(style=item.style)
-    for i in related:
-        print(i.image)
     context = {
         'item': item,
         'related': related,
