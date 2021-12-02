a95e1b1a51cba45404b787f14fc9cfbbf414c370
Raivis80 <rapet80@gmail.com>
2021-12-02 13:28:37 +0000

    Fixed sorting and quering by category functionality with proper navigation linkage and active links

--- shop/views.py
+++ shop/views.py
@@ -8,35 +8,48 @@ def shop(request):
     """A view to return the shop page"""
     products = Product.objects.all()
     categories = Category.objects.all()
+    style_list = products.values('style').distinct()
     sortkey = None
-    cat = 'hats'
+    sort_name = None
+    style = 'select'
+    category  = 'all'
     shop = True
-
     if request.GET:
         if 'category' in request.GET:
-            cat = request.GET['category']
-            if cat == 'sale':
-                products = Product.objects.filter(sale=True)
-            elif cat == 'hats':
-                products = Product.objects.all()
+            category = request.GET['category']
+            if category == 'sale':
+                products = products.filter(sale=True)
+            elif category == 'all':
+                products = products.all()
             else:
-                products = Product.objects.filter(category__name=cat)
+                products = products.filter(category__name=category)
+        if 'style' in request.GET:
+            style = request.GET['style']
+            if style != 'select':
+                products = products.filter(style=style)
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             if sortkey == 'name_asc':
+                sort_name = 'Name (A-Z)'
                 products = products.order_by('name')
             elif sortkey == 'name_desc':
+                sort_name = 'Name (Z-A)'
                 products = products.order_by('-name')
             elif sortkey == 'price_asc':
+                sort_name = 'Price (L-H)'
                 products = products.order_by('price')
             elif sortkey == 'price_desc':
+                sort_name = 'Price (H-L)'
                 products = products.order_by('-price')
     context = {
         'categories': categories,
         'products': products,
-        'current_cat': cat,
-        'shop': shop,
+        'cat': category,
         'sortkey': sortkey,
+        'sort_name': sort_name,
+        'style_list': style_list,
+        'style': style,
+        'shop': shop,
     }
     return render(request, 'shop/shop.html', context)
 
