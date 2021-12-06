c3e81d5dac6c0b03235e22f1534604520b474664
Raivis80 <rapet80@gmail.com>
2021-12-06 11:33:24 +0000

    added margins , paddings for shop item cards. Added link to shop page from infex.html

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from shop.models import Product
 # Create your views here.
 def index(request):
     """A view to return the index page"""
-    related = Product.objects.all()
+    related = Product.objects.all().order_by('-id')[:10:1]
     context = {
         'related': related
     }
