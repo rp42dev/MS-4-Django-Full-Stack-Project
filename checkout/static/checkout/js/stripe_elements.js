3cdf958a89850011026405601c5246853c9f419a
Raivis80 <rapet80@gmail.com>
2021-12-28 20:47:31 +0000

    added basic stripe payment functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,10 +1,10 @@
 // Stripe elements
 // Get stripe public and client keys
-const stripe_public_key = document.querySelector('#id_stripe_public_key').innerText.slice(1, -1);
-const client_secret = document.querySelector('#id_client_secret').innerText.slice(1, -1);
+const stripePublicKey = document.querySelector('#id_stripe_public_key').innerText.slice(1, -1);
+const clientSecret = document.querySelector('#id_client_secret').innerText.slice(1, -1);
 
 // The items the customer wants to buy
-const stripe = Stripe(stripe_public_key);
+const stripe = Stripe(stripePublicKey);
 
 // Disable the button until we have Stripe set up on the page
 document.querySelector("button").disabled = true;
@@ -26,7 +26,6 @@ var style = {
         fontFamily: 'Montserrat, sans-serif',
         color: "#d2042d",
         iconColor: "#d2042d",
-        border: '1px solid #d2042d',
     }
 };
 
@@ -42,9 +41,36 @@ card.on("change", function (event) {
     document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
 });
 
-var form = document.getElementById("payment-form");
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Complete payment when the submit button is clicked
-    payWithCard(stripe, card, data.clientSecret);
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({
+        'disabled': true
+    });
+    document.querySelector('#btn-submit').disabled = true;
+    // If the client secret was rendered server-side as a data-secret attribute
+    // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+            billing_details: {
+                name: 'Jenny Rosen'
+            }
+        }
+    }).then(function (result) {
+        if (result.error) {
+            // Show error to your customer (for example, insufficient funds)
+            document.querySelector("#card-error").textContent = result.error ? result.error.message : "";
+            card.update({
+                'disabled': false
+            });
+            document.querySelector('#submit').disabled = false;
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
--- checkout/views.py
+++ checkout/views.py
@@ -92,12 +92,21 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     context = {
         'form2': form2,
         'form': form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test client secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
