100c19fb1c6ed1878cf5658ab7baaeb007252ca1
Raivis80 <rapet80@gmail.com>
2022-01-13 16:00:36 +0000

    fixes issues with images in admin manage and js issue for side scrooll | removed subheadings

--- home/static/js/scrool-x.js
+++ home/static/js/scrool-x.js
@@ -1,10 +1,8 @@
 
 // scrool horisontal
-if (window.scrollY > 300) {
 const scrolling = document.querySelector(".scroll-wrapper");
 scrolling.scrollLeft = 130;
 scrolling.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrolling.scrollLeft += evt.deltaY;
 });
-}
