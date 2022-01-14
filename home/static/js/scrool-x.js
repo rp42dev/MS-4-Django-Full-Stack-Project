336b6c821310a015df53829c61b2bf98e9a101c6
Raivis80 <rapet80@gmail.com>
2022-01-14 11:56:49 +0000

    fixed related items gallery fiture

--- home/static/js/scrool-x.js
+++ home/static/js/scrool-x.js
@@ -1,11 +1,9 @@
 
 const scrolling = document.querySelector(".scroll-wrapper");
-scrolling.scrollLeft = 130;
-// side scrool function
 
+// side scrool function
 let left= false;
 let right = false;
 
-document.querySelector('#left').addEventListener('mousedown', () => scrolling.scrollLeft += 300);
-
-document.querySelector("#right").addEventListener('mousedown', () => scrolling.scrollLeft -= 300);
+document.querySelector('#left').addEventListener('mousedown', () => scrolling.scrollLeft -= 250);
+document.querySelector("#right").addEventListener('mousedown', () => scrolling.scrollLeft += 250);
